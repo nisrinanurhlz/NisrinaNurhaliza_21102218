66ea40856c251baf8d7aec92d8b1d5db335d6a31
nisrinanurhlz <nisrinanurhaliza2803@gmail.com>
2023-05-03 15:38:25 +0700

    My first commit

---
+++ src/com/NisrinaNurhaliza/PBO/Pertemuan5/Barang.java
@@ -0,0 +1,22 @@
+package com.NisrinaNurhaliza.PBO.Pertemuan5;
+
+public class Barang {
+    //Attribute
+    String nama;
+    int harga;
+
+    //Constructor
+    public Barang() {}
+
+    public Barang(String nama, int harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    //Method
+    public void showInfo(){
+        System.out.println("Nama Barang  : " + this.nama);
+        System.out.println("Harga Barang : " + this.harga);
+        System.out.println();
+    }
+}
